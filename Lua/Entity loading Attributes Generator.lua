ded8ddd0f2bf0545e88aa473e692a2e3a8aa38cd
shinespeciall <648616375@qq.com>
2018-07-19 17:46:57 +0800

    change the way to show information

--- Lua/Entity loading Attributes Generator.lua
+++ Lua/Entity loading Attributes Generator.lua
@@ -1,6 +1,6 @@
 local i, tmpEntityNum;
 while true do
-	if memory.readbyte(0x03000964) == 255 then
+	if memory.readbyte(0x03000964) == 255 or memory.readbyte(0x03000964) == 0 then
 		gui.text(16,0,"Running", 0xFFFF0000, topleft)
 	else
 		-- gui.text(16,0,"Proceeding", 0xFFFF0000, topleft)
@@ -19,9 +19,17 @@ while true do
 			local entityy = memory.readbyte(0x0300010c + i * 0x2C) + 255 * memory.readbyte(0x0300010d + i * 0x2C)
 			local entitysourcex = memory.readbyte(0x03000964 + i * 3 + 1) * 64
 			local entitysourcey = memory.readbyte(0x03000964 + i * 3) * 64
+			local entityOAMprt = memory.readbyte(0x03000108 + i * 0x2C) + 256 * memory.readbyte(0x03000109 + i * 0x2C) + 256 * 256 * memory.readbyte(0x0300010A + i * 0x2C)
+			local entityLayer = memory.readbyte(0x0300011E + i * 0x2C)
+			local entityPalette = memory.readbyte(0x0300011F + i * 0x2C)
+			local entityPaletteoffset = memory.readbyte(0x0300011D + i * 0x2C)
 			gui.text(0, 16 * i, tostring(entityid), 0xFFFF0000, topleft)
-			gui.text(20, 16 * i, tostring(entityx - entitysourcex), 0xFFFF0000, topleft)
+			gui.text(30, 16 * i, tostring(entityx - entitysourcex), 0xFFFF0000, topleft)
 			gui.text(68, 16 * i, tostring(entityy - entitysourcey), 0xFFFF0000, topleft)
+			gui.text(120, 16 * i, tostring(entityOAMprt), 0xFFFF0000, topleft)
+			gui.text(200, 16 * i, tostring(entityLayer), 0xFFFF0000, topleft)
+			gui.text(240, 16 * i, tostring(entityPalette), 0xFFFF0000, topleft)
+			gui.text(280, 16 * i, tostring(entityPaletteoffset), 0xFFFF0000, topleft)
 			i=i+1
 		end
 	end
